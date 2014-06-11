3bfd03d4c5a88d7b503ae07880689ffbfa7cc0ef
Vishal25 <vishalchauthmal.com>
2014-06-11 18:24:55 +0530

    Layout, CSS, layout links and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
 	subject { page }
 
 	describe "Home page" do
-		before { visit '/static_pages/home' }
+		before { visit root_path }
 
 		it { should have_content('Home') }
 		it { should have_title(full_title('')) }
@@ -13,21 +13,21 @@ describe "Static pages" do
 	end
 
 	describe "Help page" do
-		before { visit '/static_pages/help' }
+		before { visit help_path }
 
 		it { should have_content('Help') }
 		it { should have_title(full_title('Help')) }
 	end
 
 	describe "should have content 'About' " do
-		before { visit '/static_pages/about' }
+		before { visit about_path }
 
 		it { should have_content('About') }
 		it { should have_title(full_title('About')) }
 	end
 
 	describe "should have content 'Contact' " do
-		before { visit '/static_pages/contact' }
+		before { visit contact_path }
 
 		it { should have_content('Contact') }
 		it { should have_title(full_title('Contact')) }
