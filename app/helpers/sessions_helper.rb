0fd41f88777372e1ffca94fc5656eab4df675fb7
Vishal25 <vishalchauthmal.com>
2014-06-16 12:59:43 +0530

    Try to get rid of errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
 
 	def sign_in(user)
-		remember_token = User.new_remember_token
-		cookies.permanent[:remember_token] = remember_token
-		user.update_attribute(:remember_token, User.digest(remember_token))
+		rem_token = User.new_remember_token
+		cookies.permanent[:remember_token] = rem_token
+		user.update_attribute(:remember_token, User.digest(rem_token))
 		self.current_user = user
 	end
 
