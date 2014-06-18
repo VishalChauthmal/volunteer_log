38c896cf5144f610245a12a2b74999813cc80040
Vishal25 <vishalchauthmal.com>
2014-06-19 03:11:45 +0530

    Finish user editing, updating and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+
 	def new
 		@user = User.new
 	end
@@ -19,9 +22,37 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			# Handle successful update
+			flash[:success] = "Profile Updated!"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
 	private
 
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		# before filters
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, notice: "Please sign in."		
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to root_url unless current_user?(@user)	
 		end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,4 +50,42 @@ describe "User pages" do
 			end
 		end
 	end
+
+	describe "Edit" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+			visit edit_user_path(user)
+		end
+
+		describe "Page" do
+			it { should have_content('Update Your Profile') }
+			it { should have_title('Edit Profile') }
+		end
+
+		describe "with invalid information" do
+			before { click_button "Save Changes" }
+
+			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			let(:new_name) { "New Vol" }
+			let(:new_email) { "newvol@atmaexample.com" }
+
+			before do
+				fill_in "Name", with: new_name
+				fill_in "Email", with: new_email
+				fill_in "Password", with: user.password
+				fill_in "Confirm Password", with: user.password
+				click_button "Save Changes"
+			end
+
+			it { should have_title(new_name) }
+			it { should have_selector('div.alert.alert-success') }
+			it { should have_link('Sign Out', href: signout_path) }
+			specify { expect(user.reload.name).to eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
+		end
+	end
 end
